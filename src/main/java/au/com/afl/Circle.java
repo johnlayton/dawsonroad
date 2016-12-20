0c46f2c8318ff87e25afa0fd69188cd5523ae708
john layton <john.layton@touchcorp.com>
2016-12-21 06:41:13 +1100

    add test and main code

---
+++ src/main/java/au/com/afl/Circle.java
@@ -0,0 +1,103 @@
+package au.com.afl;
+
+import com.google.common.base.Preconditions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+class Circle {
+
+    private final int players;
+
+    public Circle(final int players) {
+        Preconditions.checkArgument(players > 1);
+        this.players = players;
+    }
+
+    public static void main(final String... args) {
+        Preconditions.checkArgument(args.length == 2);
+        final int players = Integer.parseInt(args[0]);
+        final int count = Integer.parseInt(args[2]);
+
+        final Result result = new Circle(players).play(count);
+
+        System.out.println(String.format("Winner = %d, Losers = %s",
+                                         result.getWinner(), IntStream.of(result.getLosers())));
+    }
+
+    public Result play(final int count) {
+
+        Preconditions.checkArgument(count > 0);
+
+        final List<Integer> eliminated = new ArrayList<>();
+
+        Player prev = link(players);
+        Player curr = prev.next();
+
+        for (int i = 1; i < players; i++) {
+            for (int j = 1; j < count; j++) {
+                prev = curr;
+                curr = curr.next();
+            }
+            prev.next(curr.next());
+            eliminated.add(curr.getId());
+            curr = prev.next();
+        }
+
+        return new Result(curr.getId(), eliminated.stream().mapToInt(i -> i).toArray());
+    }
+
+    private Player link(final int n) {
+        final Player first;
+        Player curr;
+
+        first = curr = new Player(1);
+        for (int id = 2; id <= n; id++) {
+            curr = curr.next(new Player(id));
+        }
+        curr.next(first);
+        return curr;
+    }
+
+    static class Result {
+        private final int winner;
+        private final int[] losers;
+
+        Result(int winner,
+               int[] losers) {
+            this.winner = winner;
+            this.losers = losers;
+        }
+
+        public int getWinner() {
+            return winner;
+        }
+
+        public int[] getLosers() {
+            return losers;
+        }
+    }
+
+    private class Player {
+        private final int id;
+        private Player next;
+
+        Player(final int id) {
+            this.id = id;
+        }
+
+        int getId() {
+            return id;
+        }
+
+        Player next() {
+            return next;
+        }
+
+        Player next(Player next) {
+            return this.next = next;
+        }
+    }
+}
+
---
+++ src/test/java/au/com/afl/CircleTest.java
@@ -0,0 +1,56 @@
+package au.com.afl;
+
+import au.com.afl.Circle.Result;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(DataProviderRunner.class)
+public class CircleTest {
+
+    @DataProvider
+    public static Object[][] games() {
+        return new Object[][] {
+                { 2, 1, 2, new int[] { 1 } },
+                { 2, 2, 1, new int[] { 2 } },
+                { 3, 1, 3, new int[] { 1, 2 } },
+                { 3, 2, 3, new int[] { 2, 1 } }
+        };
+    }
+
+    @Test
+    @UseDataProvider("games")
+    public void shouldCalculateWinnerAndLosers(final int players,
+                                               final int count,
+                                               final int winner,
+                                               final int[] losers) throws Exception {
+        final Result result = new Circle(players).play(count);
+
+        assertThat(result.getWinner()).isEqualTo(winner);
+        assertThat(result.getLosers()).isEqualTo(losers);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldValidatePlayers() throws Exception {
+        new Circle(1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldValidateCount() throws Exception {
+        new Circle(2).play(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldValidateMainPlayers() throws Exception {
+        Circle.main();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldValidateMainCount() throws Exception {
+        Circle.main("2");
+    }
+}
